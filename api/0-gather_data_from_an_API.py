5d254b86216a060f7714f9c0ccd4c9638c4125f3
gerberth92 <gerberthcontreras@gmail.com>
2024-04-27 12:30:50 -0500

    task 0

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Este modulo obtiene datos de una api. """
+
+import requests
+from sys import argv
+
+
+def api():
+    """ Esta funcion hace una consulta a una api. """
+
+    url = "https://jsonplaceholder.typicode.com/users/"
+
+    user = requests.get(url + argv[1]).json()
+    todo = requests.get(url + argv[1] + '/todos').json()
+    name = user['name']
+    contador = 0
+    tareas_completas = []
+
+    for item in todo:
+        contador += 1
+        if item['completed'] is True:
+            tareas_completas.append(item['title'])
+
+    print(f"Employee {name} is done with tasks"
+          f"({len(tareas_completas)}/{contador}):")
+
+    print("\t " + "\n\t ".join(tareas_completas))
+
+
+if __name__ == '__main__':
+    api()
