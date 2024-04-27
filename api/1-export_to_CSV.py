ce10dc00a3dc3209ab1707e154c545028b5a1abf
gerberth92 <gerberthcontreras@gmail.com>
2024-04-27 17:23:37 -0500

    task1

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Este modulo obtiene datos de una api. """
+
+import requests
+from sys import argv
+
+
+def api():
+    """ Esta funcion hace una consulta a una api. """
+
+    url = "https://jsonplaceholder.typicode.com/users/"
+
+    user = requests.get(url + argv[1]).json()
+    todo = requests.get(url + argv[1] + '/todos').json()
+    name = user.get('name')
+    doc = str(user.get('id')) + ".csv"
+    a = ""
+
+    with open(doc, 'a', encoding='UTF-8') as file:
+        for item in todo:
+            file.write('"{}","{}","{}","{}"\n'.format(user.get('id'), name,
+                                                      item.get('completed'),
+                                                      item.get('title')))
+
+
+if __name__ == '__main__':
+    api()
